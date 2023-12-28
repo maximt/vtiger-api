f188cbc4c10c5a6e2dd7277f3d61f535f83b7b03
maximt <vbmaxim@gmail.com>
2023-12-28 13:49:55 +0700

    update entry

--- src/controller.php
+++ src/controller.php
@@ -24,6 +24,37 @@ function action_get_contact() {
     view('contact', $data);
 }
 
+function action_post_contact() {
+    $id = preg_replace('/\W/', '', $_POST['id']);
+    if (empty($id)) {
+        action_error(403);
+    }
+
+    $filter_flags = FILTER_SANITIZE_STRING | FILTER_SANITIZE_FULL_SPECIAL_CHARS;
+    $fields = [
+        'id' => $filter_flags,
+        'lastname' => $filter_flags,
+        'firstname' => $filter_flags,
+        'mobile' => $filter_flags,
+        'assigned_user_id' => $filter_flags,
+    ];
+
+
+    $input = filter_input_array(INPUT_POST, $fields);
+
+    save_contact($id, $input);
+
+    redirect('contacts');
+}
+
+function action_delete_contact() {
+    $id = preg_replace('/\W/', '', $_POST['id']);
+    if (empty($id)) {
+        action_error(403);
+    }
+
+    echo "action_post_delete_contact: {$id}";
+}
 function action_get_about() {
     echo 'action_get_about: This is the about page.';
 }
--- src/model.php
+++ src/model.php
@@ -28,8 +28,15 @@ function get_contact($id) {
     return ['contact' => $contact, 'users' => $users];    
 }
 
-function save_contact() {
-    
+function save_contact($id, $data) {
+    $client = new Vtiger($_ENV['API_URL'],$_ENV['API_USERNAME'], $_ENV['API_ACCESSKEY']);
+
+    $client->login();
+
+    $client->update($id, $data);
+
+    $client->logout();
+
 }
 
 function delete_contact() {
